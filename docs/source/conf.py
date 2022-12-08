36ceed2b446df7fb75896f8a407f1e37bbb8147c
rkuzya <45091928+rkuzya@users.noreply.github.com>
2022-12-08 11:12:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OpenBIS Quick Start'
+copyright = '2022, Kuzyakiv'
+author = 'Kuzyakiv'
 
 release = '0.1'
 version = '0.1.0'
